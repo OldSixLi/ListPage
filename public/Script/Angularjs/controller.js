3895ff5b0d1d0326d4e8a861cfe4801cb1f7cdec
LiLaoLiu <1030809514@qq.com>
2017-04-06 17:01:09 +0800

    .

--- public/Script/Angularjs/controller.js
+++ public/Script/Angularjs/controller.js
@@ -1,113 +1,49 @@
-// /**
-//  * 控制层
-//  * @马少博 (1030809514@qq.com)
-//  * @时间  2017年4月5日13:54:19
-//  * @version 1.0
-//  */
+/**
+ * 控制层
+ * @马少博 (1030809514@qq.com)
+ * @时间  2017年4月5日13:54:19
+ * @version 1.0
+ */
+app.controller('customersCtrl', function($scope, httpService) {
+  $scope.page = function() {
+    console.log("page()方法");
+    httpService.pageing(0)
+      .then(
+        //请求成功操作
+        function(res) {
+          switch (res.status) {
+            case 200:
+              {
+                //请求成功操作
+                var data = res.data;
+                $scope.data = data;
+                $scope.dataLengths = data.content.length > 0;
+                break;
+              }
+            case 404:
+              {
+                //请求地址不存在
+                break;
+              }
+            case 500:
+              {
+                //服务器内部错误
+                console.log(JSON.stringify(res));
+                alert(res.data.errorMessage);
+                break;
+              }
+            default:
+              {
+                //请求服务失败
+                alert("请求服务失败");
+                break;
+              }
+          }
+        }
+      );
+  }
 
+  var page = $scope.page;
+  page();
 
-// app.controller('customersCtrl', ['myFactory', function($scope, myFactory) {
-//   //   $scope.page = function() {
-//   $scope.page = function() {
-//     myFactory.pageing(0, "")
-//       .then(function(res) {
-//         switch (res.status) {
-//           case 200:
-//             {
-//               var str = JSON.stringify(res);
-//               alert(str);
-//               // $scope.message = "登录成功";
-//               // save_key("userName", $scope.user.name);
-//               // save_key("password", $scope.user.password);
-//               // localStorage.accessKey = res.data.mtoken.accessKey;
-//               // localStorage.securityKey = res.data.mtoken.securityKey;
-//               // $state.go('main.home');
-//               break;
-//             }
-//           case 403:
-//             {
-//               var str = JSON.stringify(res);
-//               alert(str);
-//               // $scope.promptMes = "登录失败，请检查用户名和密码是否正确！";
-//               // $('#messages_model').modal('show');
-//               break;
-//             }
-//           case 404:
-//             {
-//               var str = JSON.stringify(res);
-//               alert(str);
-//               // $scope.promptMes = "登录失败，用户名不存在！";
-//               // $('#messages_model').modal('show');
-//               break;
-//             }
-//           default:
-//             {
-//               var str = JSON.stringify(res);
-//               alert(str);
-//               // $scope.promptMes = "登录失败，请检查网络连接！";
-//               // $('#messages_model').modal('show');
-//               break;
-//             }
-//         }
-//       });
-//   };
-
-
-// }]);
-
-// var app = angular.module('myApp', []);
-// app.controller('customersCtrl', function($scope, myFactory) {
-//   $scope.page = function() {
-//     myFactory.alerts();
-//     myFactory.pageing(0, "").then(function(res) {
-//       switch (res.status) {
-//         case 200:
-//           {
-//             var str = JSON.stringify(res);
-//             alert(str);
-//             // $scope.message = "登录成功";
-//             // save_key("userName", $scope.user.name);
-//             // save_key("password", $scope.user.password);
-//             // localStorage.accessKey = res.data.mtoken.accessKey;
-//             // localStorage.securityKey = res.data.mtoken.securityKey;
-//             // $state.go('main.home');
-//             break;
-//           }
-//         case 403:
-//           {
-//             var str = JSON.stringify(res);
-//             alert(str);
-//             // $scope.promptMes = "登录失败，请检查用户名和密码是否正确！";
-//             // $('#messages_model').modal('show');
-//             break;
-//           }
-//         case 404:
-//           {
-//             var str = JSON.stringify(res);
-//             alert(str);
-//             // $scope.promptMes = "登录失败，用户名不存在！";
-//             // $('#messages_model').modal('show');
-//             break;
-//           }
-//         default:
-//           {
-//             var str = JSON.stringify(res);
-//             alert(str);
-//             // $scope.promptMes = "登录失败，请检查网络连接！";
-//             // $('#messages_model').modal('show');
-//             break;
-//           }
-//       }
-//     });
-
-
-//   }
-// });
-
-
-// // //创建控制器
-// // app.controller('customersCtrl', function($scope, myFactory) {
-// //   myFactory.setAge(20);
-// //   $scope.r = myFactory.getAge();
-// //   alert(myFactory.name);
-// // });
+});
--- public/Script/Angularjs/service.js
+++ public/Script/Angularjs/service.js
@@ -1,65 +1,42 @@
 /**
- * 服务层
+ * 创建HTTP 请求的服务
  * @马少博 (1030809514@qq.com)
  * @时间  Date
- * @version ID
+ * @version 1.0
  */
-// var app = angular.module('myApp', []);
-// app.factory('myFactory',
-//   function($http, $q) {
-//     var defer = $q.defer();
-//     var pageing = function(pageindex, params) {
-//       alert("21232");
-//       var url = "http://60.205.170.209:8080/admin/api/" + "payment?" + params + "page=" + pageindex + "&pageNum=10"; //请求的参数和地址
-//       $http.get(url)
-//         .success(function(data, status, headers, config) {
-//           if (data != null && data != "" && data != "null") {
-//             //判断当前是否存在记录
-//             $scope.dataLengths = data.content.length > 0;
-//             if (data.content != null && data.content.length > 0) {
-//               //赋值操作 
-//               $scope.data = data;
-//             } else {
-//               alert("未获取到数据");
-//             }
 
-//           }
-//           var res = {
-//             data: data,
-//             status: status
-//           };
-//           defer.resolve(res);
-//         })
-//         .error(function(data, status, headers, config) {
-//           var res = {
-//             data: data,
-//             status: status
-//           };
-//           defer.resolve(res);
-//         });
-//       return defer.promise;
-//     };
-//     return {
-//       pageing: pageing
-//     };
+//声明APP
+var app = angular.module('myApp', []);
+//创建
+app.factory('httpService',
+  function($http, $q) {
+    var pageing = function(pageindex, url, params) {
+      // $q是Angular的一种内置服务， 它可以使你异步地执行函数， 并且当函数执行完成时或异常时它允许你使用函数的返回值或返回执行状态通知等。
+      var defer = $q.defer();
+      var url = "/users/users" + "?" + "page=" + pageindex + "&pageNum=10"; //请求的参数和地址
+      $http.get(url)
+        .success(function(data, status, head, config) {
+          var res = {
+            data: data,
+            status: status,
+            config: config
+          };
+          defer.resolve(res);
+        })
+        .error(function(data, status, headers, config) {
+          var res = {
+            data: data,
+            status: status
+          };
+          defer.resolve(res);
+        });
+      //返回数据
+      return defer.promise;
+    };
+    //返回的方法
+    return {
+      pageing: pageing
+    };
 
-//   }
-// );
-
-
-// var app = angular.module('myApp', []);
-
-// //通过工厂模式创建自定义服务
-// app.factory('myFactory', function() {
-//   var service = {}; //定义一个Object对象'
-//   service.name = "张三";
-//   var age; //定义一个私有化的变量
-//   //对私有属性写getter和setter方法
-//   service.setAge = function(newAge) {
-//     age = newAge;
-//   }
-//   service.getAge = function() {
-//     return age;
-//   }
-//   return service; //返回这个Object对象
-// });
+  }
+);
