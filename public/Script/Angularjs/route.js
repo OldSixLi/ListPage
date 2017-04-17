10cd7ca316268365b86074064e08ec21abb06582
LiLaoLiu <1030809514@qq.com>
2017-04-17 16:37:02 +0800

    .

--- public/Script/Angularjs/route.js
+++ public/Script/Angularjs/route.js
@@ -6,7 +6,7 @@
  * @Last Modified time:2017年4月7日15:28:08
  */
 var app = angular.module('myApp', ['ngRoute'])
-  .config(['$routeProvider', function ($routeProvider) {
+  .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: "../showDoc/image.html"
@@ -18,9 +18,13 @@ var app = angular.module('myApp', ['ngRoute'])
         templateUrl: "../showDoc/info.html"
       })
       .when('/form', {
-        templateUrl: "../showDoc/formAdd.html"
-      }).when('/upload', {
+        templateUrl: "../showDoc/form.html"
+      })
+      .when('/upload', {
         templateUrl: "../showDoc/upload.html"
+      })
+      .when('/talk', {
+        templateUrl: "../showDoc/talk.html"
       })
       .otherwise({
         redirectTo: '/'
